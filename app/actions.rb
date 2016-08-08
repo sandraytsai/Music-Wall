9b8722462be4d01851d408f095f61bdef03cf54d
Sandra Tsai <sandraytsai@gmail.com>
2016-08-08 16:01:32 +0000

    submit form added

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,12 @@
 get '/' do
   erb :index
 end
+
+get '/tracks' do
+  @tracks = Track.all
+  erb :'tracks/index'
+end 
+
+get '/tracks/new' do
+  erb :'messages/new'
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160808153741) do
+
+  create_table "tracks", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.string "url"
+    t.date   "created_at"
+    t.date   "updated_at"
+  end
 
 end
