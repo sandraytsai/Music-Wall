77ab1eebc4e18bbb8331c1b40e761b328d9a5b62
Sandra Tsai <sandraytsai@gmail.com>
2016-08-08 16:23:20 +0000

    tracks detail page

--- app/actions.rb
+++ app/actions.rb
@@ -9,5 +9,20 @@ get '/tracks' do
 end 
 
 get '/tracks/new' do
-  erb :'messages/new'
+  erb :'tracks/new'
 end
+
+get '/tracks/:id' do
+  @track = Track.find params[:id]
+  erb :'tracks/show'
+end
+
+post '/tracks' do
+  @track = Track.new(
+    title: params[:title],
+    author: params[:author],
+    url: params[:url],
+    )
+  @track.save 
+  redirect '/tracks'
+end 
